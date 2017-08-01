769fab31e16d81bc2b62d3504175447027ced802
Kane <Phoboes@live.com>
2017-08-01 15:42:40 +1000

    Curl should now fetch the most current version of any library.

--- mks.sh
+++ mks.sh
@@ -28,75 +28,93 @@ mks () {
   do
   shopt -s nocasematch
       case ${!i} in
-        ("jq" | "jQuery") echo "Jquery v3.2.1 added. - https://code.jquery.com/jquery-3.2.1.js"; 
-        curl https://code.jquery.com/jquery-3.2.1.js > js/jQuery.js;
-        printf "\n\t\t\t<script src=\"js/jQuery.js\"></script>" >> index.html;
+        ("jq" | "jQuery") echo "jQuery added. - https://unpkg.com/jQuery"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/jQuery)";
+        curl "${url}" > jQuery.js;
+        printf "\n\t\t<script src=\"js/jQuery.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("bb" | "backbone") echo "Backbone v1.3.3 added. - https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js"; 
-         curl https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js > js/backbone.js;  
-         printf "\n\t\t\t<script src=\"js/backbone.js\"></script>" >> index.html;
+        ("bb" | "backbone") echo "Backbone added. - https://unpkg.com/backbone"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/backbone)";
+        curl "${url}" > backbone.js;
+         printf "\n\t\t<script src=\"js/backbone.js\"></script>" >> index.html;
          echo "-------------------------------------";;
 
-        ("us" | "underscore") echo "Underscore v 1.8.3 added. - https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"; 
-        curl https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js > js/underscore.js 
-        printf "\n\t\t\t<script src=\"js/underscore.js\"></script>" >> index.html;
+        ("us" | "underscore") echo "Underscore added. - https://unpkg.com/underscore"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/underscore)";
+        curl "${url}" > underscore.js;
+ 
+        printf "\n\t\t<script src=\"js/underscore.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("d3") echo "D3 v4 added. - https://d3js.org/d3.v4.min.js"; 
-        curl https://d3js.org/d3.v4.min.js > js/d3.js;
-        printf "\n\t\t\t<script src=\"js/d3.js\"></script>" >> index.html;
+        ("d3") echo "D3 added. - https://unpkg.com/d3."; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/d3)";
+        curl "${url}" > d3.js;
+        printf "\n\t\t<script src=\"js/d3.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("3JS" | "3" | "three") echo "3JS added. - https://threejs.org/build/three.min.js"; 
-        curl https://threejs.org/build/three.min.js > js/THREE.js;
-        printf "\n\t\t\t<script src=\"js/THREE.js\"></script>" >> index.html;
+        ("3JS" | "3" | "three") echo "3JS added. - https://unpkg.com/three"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/three)";
+        curl "${url}" > THREE.js;
+        printf "\n\t\t<script src=\"js/THREE.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("react") echo "React v1.5 added. But why? - https://unpkg.com/react@15/dist/react.js"; 
-        curl https://unpkg.com/react@15/dist/react.js > js/react.js;
-        printf "\n\t\t\t<script src=\"js/react.js\"></script>" >> index.html;
+        ("react") echo "React added. But why? - https://unpkg.com/react/dist/react.js"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/react/dist/react.js)";
+        curl "${url}" > react.js;
+        printf "\n\t\t<script src=\"js/react.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("reactdom" | "rctdm" | "dom" | "rdom") echo "ReactDOM v1.5 added. - https://unpkg.com/react-dom@15/dist/react-dom.js";
-        curl https://unpkg.com/react-dom@15/dist/react-dom.js > js/React.js;
-        printf "\n\t\t\t<script src=\"js/React.js\"></script>" >> index.html;
+        ("reactdom" | "rctdm" | "dom" | "rdom") echo "ReactDOM added. - https://unpkg.com/react-dom/dist/react-dom.js";
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/react-dom/dist/react-dom.js)";
+        curl "${url}" > react-dom.js;
+        printf "\n\t\t<script src=\"js/react-dom.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("redux") echo "Redux v3.6 added. - https://cdnjs.cloudflare.com/ajax/libs/redux/3.6.0/redux.min.js"; 
-        curl https://cdnjs.cloudflare.com/ajax/libs/redux/3.6.0/redux.min.js > js/redux.js;
-        printf "\n\t\t\t<script src=\"js/redux.js\"></script>" >> index.html;
+        ("redux") echo "Redux added. - https://unpkg.com/redux/dist/redux"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/redux/dist/redux)";
+        curl "${url}" > redux.js;
+        printf "\n\t\t<script src=\"js/redux.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("tm" | "tweenmax" | "tmax") echo "Redux v3.6 added. - http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"; 
-        curl http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js > js/tweenmax.js;
-        printf "\n\t\t\t<script src=\"js/tweenmax.js\"></script>" >> index.html;
+        ("gsap" | "greensocks" | "greensock" | "green" | "gs" | "green_socks" | "green_sock") echo "GreenSock added. - https://unpkg.com/gsap"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/gsap)";
+        curl "${url}" > greensock.js;
+        printf "\n\t\t<script src=\"js/greensock.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("ember" | "mbr") echo "Ember v 2.11 added. - https://cdnjs.cloudflare.com/ajax/libs/ember.js/2.11.2/ember.min.js"; 
-        curl https://cdnjs.cloudflare.com/ajax/libs/ember.js/2.11.2/ember.min.js > js/ember.js;
-        printf "\n\t\t\t<script src=\"js/ember.js\"></script>" >> index.html;
+        ("ember" | "mbr") echo "Ember added. - https://unpkg.com/ember-source/dist/ember.min.js"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/ember-source/dist/ember.min.js)";
+        curl "${url}" > ember.js;
+        printf "\n\t\t<script src=\"js/ember.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("paper" | "pjs" | "paperjs") echo "PaperJS v 0.10.3 added. - https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.10.3/paper-full.min.js"; 
-        curl https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.10.3/paper-full.min.js >> js/paper.js
-        printf "\n\t\t\t<script src=\"js/paper.js\"></script>" >> index.html;
+        ("paper" | "pjs" | "paperjs") echo "PaperJS added. - https://unpkg.com/paper/dist/paper-full.js"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/paper/dist/paper-full.js)";
+        curl "${url}" > paper.js;
+        printf "\n\t\t<script src=\"js/paper.js\"></script>" >> index.html;
         echo "-------------------------------------";;
 
-        ("vue") echo "Vue v 2.11 added. - https://cdnjs.cloudflare.com/ajax/libs/vue/2.2.1/vue.min.js"; 
-        curl https://cdnjs.cloudflare.com/ajax/libs/vue/2.2.1/vue.min.js > js/vue.js
-        printf "\n\t\t\t<script src=\"js/vue.js\"></script>" >> index.html;;
+        ("phaser" | "phsr" ) echo "Phaser 2 added. - https://unpkg.com/phaser/dist/p2.js"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/phaser/dist/p2.js)";
+        curl "${url}" > phaser2.js;
+        printf "\n\t\t<script src=\"js/phaser2.js\"></script>" >> index.html;
+        echo "-------------------------------------";;
+
+        ("vue") echo "Vue added. - https://unpkg.com/vue/dist/vue.js"; 
+        url="$(curl -Ls -o /dev/null -w %{url_effective} https://unpkg.com/vue/dist/vue.js)";
+        curl "${url}" > vue.js;
+        printf "\n\t\t<script src=\"js/vue.js\"></script>" >> index.html;;
         (*) echo "Unknown library.";
         echo "-------------------------------------";;
       esac
     done
 
 
-     printf "\n\t\t<script src=\"js/main.js\"></script>
+    printf "\n\t\t<script src=\"js/main.js\"></script>
     <link rel=\"stylesheet\" href=\"css/style.css\">
   </head>
   <body>
-
   </body>
 </html>" >> index.html
 
